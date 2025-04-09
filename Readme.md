3208c96050d7fa870775baa5dcded6349c816bbc
Miguel Amboage <mamboagearmada@danielcastelao.org>
2025-04-09 13:54:22 +0200

    tercer refactor (puntuación normal)

--- Readme.md
+++ Readme.md
@@ -1,2 +1,3 @@
 1. if de las puntuaciones iguales
 2. else if del jugador ganador
+3. puntuación normal
--- src/Main.java
+++ src/Main.java
@@ -15,29 +15,35 @@ public class Main {
         }
         else
         {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            score = puntuación(m_score1, m_score2, score);
 
         }
     return score;
+    }
+
+    private static String puntuación(int m_score1, int m_score2, String score) {
+        int tempScore;
+        for (int i = 1; i<3; i++)
+        {
+            if (i==1) tempScore = m_score1;
+            else { score +="-"; tempScore = m_score2;}
+            switch(tempScore)
+            {
+                case 0:
+                    score +="Love";
+                    break;
+                case 1:
+                    score +="Fifteen";
+                    break;
+                case 2:
+                    score +="Thirty";
+                    break;
+                case 3:
+                    score +="Forty";
+                    break;
+            }
+        }
+        return score;
     }
 
     private static String jugadorconventaja(int m_score1, int m_score2) {
