4ff66e9875ec59213ab768206c86caf1a2c4f17e
Miguel Amboage <mamboagearmada@danielcastelao.org>
2025-04-09 13:52:39 +0200

    segundo refactor (else if del ganador)

--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
 1. if de las puntuaciones iguales
+2. else if del jugador ganador
--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,7 @@ public class Main {
         }
         else if (m_score1 >=4 || m_score2 >=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            score = jugadorconventaja(m_score1, m_score2);
         }
         else
         {
@@ -42,6 +38,16 @@ public class Main {
 
         }
     return score;
+    }
+
+    private static String jugadorconventaja(int m_score1, int m_score2) {
+        String score;
+        int minusResult = m_score1 - m_score2;
+        if (minusResult==1) score ="Advantage player1";
+        else if (minusResult ==-1) score ="Advantage player2";
+        else if (minusResult>=2) score = "Win for player1";
+        else score ="Win for player2";
+        return score;
     }
 
     private static String scores_iguales(int m_score1) {
